9b87a92792cc4397a78766a9dba5e21e883af593
Christopher Broome <67555677+cbroome17@users.noreply.github.com>
2021-05-04 07:01:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,23 +9,34 @@ def main():
         "Are you looking to 'encrypt', or 'decrypt'? \nif seeking to create first time key then enter 'key' as your input\n"
     )
     file_or_folder = input("(file) or (folder):\n")
+    
+    
     if encrypt_or_decrypt == 'encrypt':
+        
         if file_or_folder == 'file':
             file = config.load_file()
             encrypt.encrypt_file(key, file)
+            
         elif file_or_folder == 'folder':
             file = config.load_directory
             encrypt.encrypt_folder(key, file)
+            
+            
     elif encrypt_or_decrypt == 'decrypt':
+        
         if file_or_folder == 'file':
             file = config.load_file()
             decrypt.decrypt_file(key, file)
+            
         elif file_or_folder == 'folder':
-            file = config.load_file()
+            file = config.load_directory()
             decrypt.decrypt_folder(key, file)
+            
+            
     elif encrypt_or_decrypt == 'key':
         config.write_key()
 
+        
 
 if __name__ == '__main__':
     main()
